dbc602bb95e9516e2415dccbf1f5e541936b774a
Matthew <raccoonformality@gmail.com>
2023-11-03 02:34:12 -0400

    tweaking the pew pew device

--- Global.gd
+++ Global.gd
@@ -1,4 +1,7 @@
 extends Node
 
+var weaponsDict = {
+	"ar15": {"fireRate": 10, "rapid": true}
+}
 
 var useController = false
--- scripts/particles/GunInpactParticles.gd
+++ scripts/particles/GunInpactParticles.gd
@@ -1,9 +1,10 @@
 extends Spatial
 
+onready var holes = $Sprite3D
+
 func _ready():
 	$Particles.emitting = true
 
-var mat = load("res://assets/programmerArt/mat.tres")
 
 func _on_Timer_timeout():
 	queue_free()
