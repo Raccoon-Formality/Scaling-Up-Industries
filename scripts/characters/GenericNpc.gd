d63d1e0c3976ec289979ee7ce0299ef542cea774
dallen72 <12113373+dallen72@users.noreply.github.com>
2023-11-05 07:38:18 -0500

    NPC navigating with mesh nav
    To make a nav mesh for a level, follow the instructions in the first 30 minutes or so of the video here, omitting the Navigation node : https://www.youtube.com/watch?v=9yQ5jax2PIA

--- scripts/characters/GenericNpc.gd
+++ scripts/characters/GenericNpc.gd
@@ -5,12 +5,34 @@ extends KinematicBody
 
 var num_health_points
 
+var navAgent : NavigationAgent
+
+onready var target_location = get_node("../Player")
+
 func _ready():
 	$StateIndicatorTimer.wait_time = 0.25
+	navAgent = $NavigationAgent
+#	path = Navigation.get_simple_path(translation, goal.translation, true)
+
+
+var path = []
 	
+
+func _physics_process(delta):
+	if navAgent.is_navigation_finished():
+		return
+		
+	var targetPos = navAgent.get_next_location()
+	var direction = global_transform.origin.direction_to(targetPos)
+	var velocity = direction * navAgent.max_speed
+	
+	move_and_slide(velocity, Vector3.UP)	
 
 func inflict_damage():
 	print("debug - npc shot, inside enemy class method")
+	
+	var player_pos = target_location.global_transform.origin
+	navAgent.set_target_location(player_pos)
 	
 	change_state_to_alerted()
 
