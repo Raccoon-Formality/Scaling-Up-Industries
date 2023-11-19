bf58a7469a968c31c14a64b986e5bb60d79ae515
Matthew <raccoonformality@gmail.com>
2023-11-18 19:24:07 -0500

    starting part update

--- scripts/starting_level.gd
+++ scripts/starting_level.gd
@@ -17,8 +17,17 @@ var stopping = false
 
 var trainSpeed = 2.0
 var stoppingSpeed = 10.0
+var counter = 0
+
+onready var massiveCube = $building/StaticBody/buildingMesh/CSGBox/massive
 
 func _ready():
+	$building.translation = Vector3(0,-2,-45)
+	trainSpeed = 2.0
+	#massiveCube.hide()
+	started = false
+	counter = 0
+	stopping = false
 	grass.set_shader_param("speed",0.0)
 	track.set_shader_param("speed",0.0)
 	for x in range(50 - $startTrees.global_translation.z):
@@ -33,35 +42,45 @@ func _ready():
 onready var player = load("res://scenes/characters/Player.tscn")
 onready var tree = load("res://scenes/assets/3d/tree.tscn")
 
-var counter = 0
+var timingOffset = 300
 
 func _process(delta):
-	counter += 1
 	if started:
+		counter += 1
 		$moving.translation.z += 20 * delta * trainSpeed
-		if counter % 1 == 0:
+		if counter % 1 == 0 and counter < 200 + timingOffset:
 			var treeInstance = tree.instance()
 			$moving.add_child(treeInstance)
 			treeInstance.global_translation = $startTrees.global_translation
 			treeInstance.global_translation.x = rand_range($startTrees.global_translation.x, $endTrees.global_translation.x)
 			if treeInstance.global_translation.x > -5 and treeInstance.global_translation.x < 5:
 				treeInstance.queue_free()
+		elif counter == 250 + timingOffset:
+			$building.translation = Vector3(0,-2,-45)
+			massiveCube.show()
+		elif counter > 250 + timingOffset and counter < 300 + timingOffset:
+			$building.translation.z += 20 * delta * trainSpeed
+		elif counter > 300 + timingOffset:
+			stopping = true
+			grass.set_shader_param("speed",0.0)
+			track.set_shader_param("speed",0.0)
+			started = false
+			get_node("Player").screenshake(0,0)
 	for object in $moving.get_children():
 		if object.global_translation.z > 50:
 			object.queue_free()
-	if Input.is_action_just_pressed("slow"):
-		stopping = true
-		grass.set_shader_param("speed",0.0)
-		track.set_shader_param("speed",0.0)
-		started = false
 	if stopping:
 		trainSpeed = lerp(trainSpeed,0.0,0.1)
 		$MeshInstance.global_translation.z += stoppingSpeed * trainSpeed * delta
 		$train_track.global_translation.z += stoppingSpeed * trainSpeed * delta
+		if !is_equal_approx($building.global_translation.z, -7.5):
+			$building.global_translation.z = lerp($building.global_translation.z, -7.5, 0.1) #stoppingSpeed * trainSpeed * delta
 		$moving.global_translation.z += stoppingSpeed * trainSpeed * delta
 		$trainSound.volume_db = linear2db(trainSpeed / 2.0)
+	print($building.global_translation.z)
 
 func startGame():
+	$trainStoppingSound.play()
 	var playerInstance = player.instance()
 	playerInstance.translation = $Camera.translation
 	playerInstance.translation.y -= 1.5
@@ -72,3 +91,4 @@ func startGame():
 	$trainSound.play()
 	grass.set_shader_param("speed",2.0 * trainSpeed)
 	track.set_shader_param("speed",3.0 * trainSpeed)
+	playerInstance.screenshake(9999999999999,0.5)
