78ac6098d6ce47a88e3e76bc32766636d20ad985
Matthew <raccoonformality@gmail.com>
2023-11-27 14:21:24 -0500

    enemies textured implemented

--- splash.gd
+++ splash.gd
@@ -1,6 +1,9 @@
 extends Control
 
-
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene("res://scenes/starting_level.tscn")
+		Global.startStartPoint = 6.0
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
